6fd0a8ff1959557302f020e61fd4e054e79d43d4
Alwis <de1999alwis@gmail.com>
2025-05-04 23:44:17 +0530

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,16 @@
-export const cart = [
-  { id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9", quantity: 4 },
-  { id: "15b6fc6f-327a-4ec4-896f-486349e85a3d", quantity: 2 },
-  { id: "54e0eccd-8f36-462b-b68a-8182611d9add", quantity: 4 },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    { id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9", quantity: 4 },
+    { id: "15b6fc6f-327a-4ec4-896f-486349e85a3d", quantity: 2 },
+    { id: "54e0eccd-8f36-462b-b68a-8182611d9add", quantity: 4 },
+  ];
+}
+
+export function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let machItem;
@@ -24,12 +32,14 @@ export function addToCart(productId) {
       quantity: getQuantity,
     });
   }
+  saveToStorage();
 }
 
 export function deleteFromCart(itemId) {
-  for (let index=0; index < cart.length; index++) {
+  for (let index = 0; index < cart.length; index++) {
     if (cart[index].id === itemId) {
       cart.splice(index, 1);
+      saveToStorage();
       return true;
     }
   }
