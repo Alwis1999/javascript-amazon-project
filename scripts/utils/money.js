83f0befffd7dec488b828e2fd77c879b8f57b09c
Alwis <de1999alwis@gmail.com>
2025-05-12 14:04:51 +0530

    remove default tests and add money tests

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,3 @@
 export function formatCurrency(centValue) {
-  return (centValue / 100).toFixed(2);
+  return (Math.round(centValue) / 100).toFixed(2);
 }
--- test/moneyTest.js
+++ test/moneyTest.js
@@ -1,5 +1,7 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
+console.log("test suit: formatCurruncy");
+
 console.log("convert cents into dollers");
 if (formatCurrency(2005) === "20.05") {
   console.log("passed");
@@ -7,14 +9,14 @@ if (formatCurrency(2005) === "20.05") {
   console.log("fail");
 }
 
-console.log("\nworks with 0");
+console.log("works with 0");
 if (formatCurrency(0) === "0.00") {
   console.log("passed");
 } else {
   console.log("fail");
 }
 
-console.log("\nround up nearst cents");
+console.log("round up nearst cents");
 if (formatCurrency(2000.5) === "20.01") {
   console.log("passed");
 } else {
